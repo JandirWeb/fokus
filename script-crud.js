9655575e802b06f05f5907fcc0a34e39bb20919f
Jandir Moreira <jandirweb@gmail.com>
2024-03-17 18:24:57 -0300

    Armazenando em local storage

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,21 @@
-//Encontrar o botão de adicionar tarefa
 const btnAddTarefa = document.querySelector('.app__button--add-task');
-
 const formAddTarefa = document.querySelector('.app__form-add-task');
+const textarea = document.querySelector('.app__form-textarea');
+
+const tarefas = [];
 
 btnAddTarefa.addEventListener('click', () => {
     formAddTarefa.classList.toggle('hidden');
+});
+
+formAddTarefa.addEventListener('submit', (evento) =>{
+    evento.preventDefault();
+
+    const tarefa = {
+        descricao: textarea.value
+    }
+
+    tarefas.push(tarefa);
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
 });
