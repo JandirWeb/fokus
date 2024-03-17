da41f8dd2b3a992fc9bde23c377d0142bcfaee0e
Jandir Moreira <jandirweb@gmail.com>
2024-03-17 18:43:21 -0300

    detalhes de criacao de elemtnos em js

--- script-crud.js
+++ script-crud.js
@@ -4,6 +4,37 @@ const textarea = document.querySelector('.app__form-textarea');
 
 const tarefas = [];
 
+function criarElementoTarefa(tarefa){
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
+
+    const svg = document.createElement('svg');
+    svg.innerHTML = `
+    <svg>
+    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+        <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+    </svg>
+</svg>
+    `;
+
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('app__section-task-list-item-description');
+    paragrafo.textContent = tarefa.descricao;
+
+    const botao = document.createElement('button');
+    botao.classList.add('app_button-edit');
+
+    const imgBotao = document.createElement('img');
+    imgBotao.setAttribute('src', '/imagens/edit.png' );
+
+    botao.append(imgBotao);
+
+    li.append(svg);
+    li.append(paragrafo);
+    li.append(botao);
+}
+
 btnAddTarefa.addEventListener('click', () => {
     formAddTarefa.classList.toggle('hidden');
 });
